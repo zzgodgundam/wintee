ca28fb2638f9e313da8efcd79560b3eb0e3aad39
zzgodgundam <zzgodgundam@gmail.com>
2020-12-10 14:26:56 +0800

    [make.bat] change build to Build.

--- make.bat
+++ make.bat
@@ -1,15 +1,15 @@
 @set OUTPUT_NAME=wtee
-@if not exist build mkdir build
-@if exist build\OutputName.h del build\OutputName.h
-@if exist build\%OUTPUT_NAME%.exe del build\%OUTPUT_NAME%.exe
+@if not exist Build mkdir Build
+@if exist Build\OutputName.h del Build\OutputName.h
+@if exist Build\%OUTPUT_NAME%.exe del Build\%OUTPUT_NAME%.exe
 
-@echo #define OUTPUT_NAME "%OUTPUT_NAME%" > build\OutputName.h
+@echo #define OUTPUT_NAME "%OUTPUT_NAME%" > Build\OutputName.h
 
-cl /nologo /c /D_CRT_SECURE_NO_WARNINGS /W3 /WX /MT /O2 /Ibuild wintee.c
+cl /nologo /c /D_CRT_SECURE_NO_WARNINGS /W3 /WX /MT /O2 /IBuild wintee.c
 @if not [%ERRORLEVEL%]==[0] goto :END
-link /nologo /OUT:build\%OUTPUT_NAME%.exe wintee.obj
+link /nologo /OUT:Build\%OUTPUT_NAME%.exe wintee.obj
 @if not [%ERRORLEVEL%]==[0] goto :END
 
-@echo %OUTPUT_NAME%.exe is generated in build folder!
+@echo %OUTPUT_NAME%.exe is generated in Build folder!
 :END
 @del *.obj
